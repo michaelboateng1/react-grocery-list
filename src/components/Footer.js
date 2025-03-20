3d563cd5a7aa7c4de855361a79862d8ff0716e1d
Michael Boateng <michaelboateng064@gmail.com>
2025-03-20 14:21:38 +0000

    Added first component layout

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy;copyright 2025. All rights reserved</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <main>
+      <article>
+        <h2>Mian Body</h2>
+      </article>
+    </main>
+  );
+}
+
+export default MainContent;
